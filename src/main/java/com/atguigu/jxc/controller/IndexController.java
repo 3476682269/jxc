2e7683b6fd507e55e111d85a0ed2521e5773628c
小憨憨解忧铺 <3476682269@qq.com>
2020-12-15 15:45:43 +0800

    Initial commit 最后一次项目实战

---
+++ src/main/java/com/atguigu/jxc/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.atguigu.jxc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @description 系统首页请求控制器
+ */
+@Controller
+@CrossOrigin
+public class IndexController {
+
+    /**
+     * 进入登录页面
+     * @return 重定向至登录页面
+     */
+    @GetMapping("/")
+    public String toIndex(){
+        return "redirect:login.html";
+    }
+}
